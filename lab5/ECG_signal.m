44ec520d1b01c17d79ee35d86a96d9eb782d863a
SamarShabanCS <ssa10@fayoum.edu.eg>
2020-12-09 01:42:56 +0200

    lab5

---
+++ lab5/ECG_signal.m
@@ -0,0 +1,8 @@
+%% load ECG signal
+
+clear; close all; clc;
+load ecg_data.mat
+t = 1:1000;
+figure(1); plot(t,x,'r','LineWidth',1.5);
+xlabel('time  ----->');ylabel('Amplitude ----->');
+title('ECG Signal');grid on;
---
+++ lab5/random_ECG.m
@@ -0,0 +1,9 @@
+
+%% random ECG signal
+
+clear; close all; clc;
+k = -6:15;
+y = randn(size(k));
+figure(); stem(k,y,'b','fill','LineWidth',1.2);
+axis([-7 16 min(y)-1 max(y)+1]);
+title('Random Sequence');
